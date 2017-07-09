a5bfc6648c795b6211917fda787937374fdfbde3
antonyCastineiras <antonycastineiras80@gmail.com>
2017-07-09 20:22:01 +0100

    works on heroku

--- app/uploaders/music_file_uploader.rb
+++ app/uploaders/music_file_uploader.rb
@@ -2,10 +2,6 @@ class MusicFileUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
   def filename
     model.name if original_filename
   end
